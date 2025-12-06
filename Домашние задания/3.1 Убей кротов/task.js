aa547395c06e7a201131e7d4e20935c2b38f7da1
Dan Miller <forpesmobile2100@gmail.com>
2025-12-06 18:14:33 +0400

    Реализовано задание 3.1

---
+++ Домашние задания/3.1 Убей кротов/task.js
@@ -0,0 +1,35 @@
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+
+function getHole(index) {
+    return document.getElementById(`hole${index}`);
+}
+
+for (let i = 1; i <= 9; i++) {
+    const hole = getHole(i);
+
+    hole.onclick = function () {
+        if (hole.classList.contains("hole_has-mole")) {
+
+            dead.textContent = Number(dead.textContent) + 1;
+        } else {
+
+            lost.textContent = Number(lost.textContent) + 1;
+        }
+
+        if (Number(dead.textContent) === 10) {
+            alert("Победа! Вы убили 10 кротов!");
+            resetGame();
+        }
+
+        if (Number(lost.textContent) === 5) {
+            alert("Игра окончена! 5 промахов.");
+            resetGame();
+        }
+    };
+}
+
+function resetGame() {
+    dead.textContent = 0;
+    lost.textContent = 0;
+}
